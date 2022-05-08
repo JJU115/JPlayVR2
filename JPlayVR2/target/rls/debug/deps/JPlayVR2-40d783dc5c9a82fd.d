f07e1bcabf353c3e5e0f655cb51396d4db98e59a
Justin Underhay <jju115@telus.net>
2022-05-08 16:57:17 -0700

    Rust project structure

---
+++ JPlayVR2/target/rls/debug/deps/JPlayVR2-40d783dc5c9a82fd.d
@@ -0,0 +1,5 @@
+c:\Users\Justin\Documents\Rust\Projects\JPlayVR2\JPlayVR2\target\rls\debug\deps\JPlayVR2-40d783dc5c9a82fd.rmeta: src\main.rs
+
+c:\Users\Justin\Documents\Rust\Projects\JPlayVR2\JPlayVR2\target\rls\debug\deps\JPlayVR2-40d783dc5c9a82fd.d: src\main.rs
+
+src\main.rs:
