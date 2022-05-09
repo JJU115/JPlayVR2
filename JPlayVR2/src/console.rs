5d8a0cc0e3d761b5f6a2d2c0609a1d8069e83671
Justin Underhay <jju115@telus.net>
2022-05-08 17:18:38 -0700

    Starting files and .gitignore

---
+++ JPlayVR2/src/console.rs
@@ -0,0 +1,15 @@
+mod console {
+
+    struct console {
+
+        fn load_cartridge() {
+
+        }
+
+
+        fn play_game() {
+            //General idea:
+            //Loop that executes a single CPU cycle then sequentially executes three PPU cycles
+        }
+    }
+}
