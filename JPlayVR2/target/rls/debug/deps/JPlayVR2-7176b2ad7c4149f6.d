986f4753604cf103b75de6fe4f724d9cc57bab18
Justin Underhay <jju115@telus.net>
2022-05-08 17:22:20 -0700

    Reorganize project files

--- JPlayVR2/target/rls/debug/deps/JPlayVR2-7176b2ad7c4149f6.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Justin\Documents\Rust\Projects\JPlayVR2\JPlayVR2\target\rls\debug\deps\JPlayVR2-7176b2ad7c4149f6.rmeta: src\main.rs
-
-c:\Users\Justin\Documents\Rust\Projects\JPlayVR2\JPlayVR2\target\rls\debug\deps\JPlayVR2-7176b2ad7c4149f6.d: src\main.rs
-
-src\main.rs:
