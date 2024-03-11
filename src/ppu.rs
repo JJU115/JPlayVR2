7220e5a37a86fab11b3aaaa01851db5741f3e04c
Justin Underhay <jju115@telus.net>
2024-03-10 21:27:21 -0700

    Functionality on writes to ppu data, addr, and scroll registers.

--- src/ppu.rs
+++ src/ppu.rs
@@ -9,7 +9,7 @@ pub mod ppu {
         secondary_oam: Vec<u8>,
         ppudata_buffer: u8,
 
-        current_scanline: u16,
+        current_scanline: i16,
         scanline_cycle: u16,
         is_odd_cycle: bool,
 
@@ -18,9 +18,9 @@ pub mod ppu {
         ppu_mask: u8,
         oam_addr: u8,
         oam_data: u8,
-        ppu_scroll: u8,
+        //ppu_scroll: u8,
         ppu_addr: u8,
-        ppu_data: u8,
+        //ppu_data: u8,
 
         vram_addr: u16, //v
         temp_vram_addr: u16, //t
@@ -45,7 +45,7 @@ pub mod ppu {
                 secondary_oam: vec![0; 64],
                 ppudata_buffer: 0,
 
-                current_scanline: 0,
+                current_scanline: -1,
                 scanline_cycle: 0,
                 is_odd_cycle: false,
 
@@ -54,9 +54,9 @@ pub mod ppu {
                 ppu_mask: 0,
                 oam_addr: 0,
                 oam_data: 0,
-                ppu_scroll: 0,
+                //ppu_scroll: 0,
                 ppu_addr: 0,
-                ppu_data: 0,
+                //ppu_data: 0,
 
                 vram_addr: 0,
                 temp_vram_addr: 0,
@@ -98,7 +98,7 @@ pub mod ppu {
                 7 => { //PPUDATA when v is before palettes
                     //Read from vram from the address specified in PPUADDR, then increment PPUADDR
                     //Utilize the internal read buffer
-                    let buffer_val = self.ppudata_buffer;
+                    let buffer_val: u8 = self.ppudata_buffer;
                     self.ppudata_buffer = self.vram[self.vram_addr as usize];
                     self.vram_addr += if self.ppu_ctrl & 0x04 == 0 {1} else {32};
                     buffer_val
@@ -118,18 +118,52 @@ pub mod ppu {
                                         value & 0x80 != 0 &&
                                         self.ppu_status & 0x80 != 0 &&
                                         self.current_scanline > 240;
-                    self.ppu_ctrl = value;
-                
+                    self.temp_vram_addr &= 0x73FF;
+                    self.temp_vram_addr |= (value as u16 & 0x03) << 10;
+                    self.ppu_ctrl = value;                
                 }
                 //PPUMASK, rendering of sprites/backgrounds enabled and disabled here
                 1 => {self.ppu_mask = value;}
                 //OAMDADDR, set to 0 during each of ticks 257–320 of the pre-render and visible scanlines
                 3 => {self.oam_addr = value;}
-                //OAMDATA, 
-                4 => {self.oam_data = value;}
-                5 => {}
-                6 => (), //Should return open bus
-                
+                //OAMDATA, best to ignore writes during the rendering period
+                4 if self.current_scanline > 239 => {
+                    self.oam_data = value;
+                    self.primary_oam[self.oam_addr as usize] = value;
+                    self.oam_addr += 1;
+                }
+                //PPUSCROLL - write toggle is false
+                5 if !self.write_toggle => {
+                    self.temp_vram_addr &= 0xFFE0;
+                    self.temp_vram_addr |= value as u16 & 0x1F;
+                    self.fine_x_scroll = value & 0x07;
+                    self.write_toggle = true;
+                },
+                //PPUSCROLL - second write
+                5 if self.write_toggle => {
+                    self.temp_vram_addr &= 0x0C1F;
+                    self.temp_vram_addr |= (value as u16 & 0x07) << 12; 
+                    self.temp_vram_addr |= (value as u16 & 0xF1) << 2;
+                    self.write_toggle = false;
+                }
+                //PPUADDR - First write, toggle is false
+                6 if !self.write_toggle => {
+                    self.temp_vram_addr &= 0x00FF;
+                    self.temp_vram_addr |= (value as u16 & 0x3F) << 8;
+                    self.write_toggle = true; 
+                },
+                //PPUADDR - Second write, toggle is true
+                6 if self.write_toggle => {
+                    self.temp_vram_addr &= 0xFF00;
+                    self.temp_vram_addr |= value as u16;
+                    self.vram_addr = self.temp_vram_addr;
+                    self.write_toggle = false;
+                },
+                //PPUDATA
+                7 => {
+                    self.vram[self.ppu_addr as usize] = value;
+                    self.vram_addr += if self.ppu_ctrl & 0x04 == 0 {1} else {32};
+                }
                 _ => ()
             }
         }
